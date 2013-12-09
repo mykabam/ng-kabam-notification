b34a797d1667f04baab4a2db0064c9be3c611ca3
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-12-10 00:57:44 +0700

    feat(*): Setup ng-kabam-notification skeleton project
    JIRA Issue: KAB-190

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: {
+        src: ['Gruntfile.js', 'ng-kabam-notification.js', 'test/**/*.js']
+      },
+      ci: {
+        options: {
+          force: true,
+          reporter: 'checkstyle',
+          reporterOutput: 'results/jshint-result.xml'
+        },
+        src: '<%= jshint.all.src %>'
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Tasks
+  grunt.registerTask('test', ['karma:unit']);
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
