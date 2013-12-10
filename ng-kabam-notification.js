ba1a92c8238ed1a974aab20dd83984b9ad9d98ff
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-12-10 15:40:32 +0700

    feat(service): requestPermission()
    Write a service to request Web Notification permission.
    
    Usage:
    
    WebNotification.requestPermission()
    
    Important notes:
    - The Google Chrome Web Notification implementation is required user gesture to call the requestPermission method
    http://www.chromium.org/developers/design-documents/desktop-notifications/api-specification
    
    JIRA Issue: KAB-192

--- ng-kabam-notification.js
+++ ng-kabam-notification.js
@@ -13,6 +13,31 @@ angular.module('ng-kabam-notification', [])
       isSupported: function() {
         var webNotifIsSupported = ('Notification' in window) ? true : false;
         return webNotifIsSupported;
+      },
+      /**
+       * To request Web Notification permission
+       */
+      requestPermission: function() {
+        var userAgent = navigator.userAgent,
+          notification = null;
+
+        var FIREFOX_USER_AGENT = userAgent.match(/Firefox\//),
+          CHROME_USER_AGENT = userAgent.match(/Chrome\//);
+
+        if (FIREFOX_USER_AGENT) {
+          notification = Notification;
+        } else if (CHROME_USER_AGENT) {
+          notification = webkitNotifications;
+        }
+
+        if (notification && notification.permission !== 'denied') {
+
+          notification.requestPermission(function(status) {
+            if (notification.permission !== status) {
+              notification.permission = status;
+            }
+          });
+        }
       }
     };
   });
