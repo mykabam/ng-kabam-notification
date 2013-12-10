12aed4974120346ba7162674b064fc00f4991b30
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-12-10 21:47:59 +0700

    feat(service): use socket service inside WebNotification factory
    The idea is to use the socket and listening to notify event.
    
    JIRA Issue: KAB-193

--- ng-kabam-notification.js
+++ ng-kabam-notification.js
@@ -25,41 +25,48 @@ angular.module('ng-kabam-notification', [
       socketProvider.ioSocket(kabamSocket);
     }
   ])
-  .factory('WebNotification', function() {
-    return {
+  .factory('WebNotification', ['socket',
+    function(socket) {
 
-      /**
-       * To check if Web Notification is supported or not in running browser.
-       * @return {Boolean} return true if Web Notification is supported
-       */
-      isSupported: function() {
-        var webNotifIsSupported = ('Notification' in window) ? true : false;
-        return webNotifIsSupported;
-      },
-      /**
-       * To request Web Notification permission
-       */
-      requestPermission: function() {
-        var userAgent = navigator.userAgent,
-          notification = null;
+      socket.on('notify', function(data) {
+        console.log('on notify data', data);
+      });
 
-        var FIREFOX_USER_AGENT = userAgent.match(/Firefox\//),
-          CHROME_USER_AGENT = userAgent.match(/Chrome\//);
+      return {
 
-        if (FIREFOX_USER_AGENT) {
-          notification = Notification;
-        } else if (CHROME_USER_AGENT) {
-          notification = webkitNotifications;
+        /**
+         * To check if Web Notification is supported or not in running browser.
+         * @return {Boolean} return true if Web Notification is supported
+         */
+        isSupported: function() {
+          var webNotifIsSupported = ('Notification' in window) ? true : false;
+          return webNotifIsSupported;
+        },
+        /**
+         * To request Web Notification permission
+         */
+        requestPermission: function() {
+          var userAgent = navigator.userAgent,
+            notification = null;
+
+          var FIREFOX_USER_AGENT = userAgent.match(/Firefox\//),
+            CHROME_USER_AGENT = userAgent.match(/Chrome\//);
+
+          if (FIREFOX_USER_AGENT) {
+            notification = Notification;
+          } else if (CHROME_USER_AGENT) {
+            notification = webkitNotifications;
+          }
+
+          if (notification && notification.permission !== 'denied') {
+
+            notification.requestPermission(function(status) {
+              if (notification.permission !== status) {
+                notification.permission = status;
+              }
+            });
+          }
         }
-
-        if (notification && notification.permission !== 'denied') {
-
-          notification.requestPermission(function(status) {
-            if (notification.permission !== status) {
-              notification.permission = status;
-            }
-          });
-        }
-      }
-    };
-  });
+      };
+    }
+  ]);
