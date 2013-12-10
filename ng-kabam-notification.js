6fb8156751970ea54a7abc93f27d0f4818ac75a6
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-12-10 15:49:27 +0700

    docs(service): add more docs about the Web Notification service
    JIRA Issue: KAB-189

--- ng-kabam-notification.js
+++ ng-kabam-notification.js
@@ -1,5 +1,18 @@
 /**
- * @overview Notification service for Kabam application
+ * @overview Web Notification service for Kabam application
+ * @ngdoc service
+ *
+ * @description service that carry out specific tasks like
+ * requesting permission, checking the notification permission status,
+ * so this Notification service kinda like a wrapper to access the
+ * Web Notification.
+ *
+ * Notes on Google Chrome usage:
+ * There's one very important caveat about requesting permission:
+ * the page can only request permission when a user initiates some action on
+ * a page, in this case, clicking on a button. So, when using this directive
+ * only click event will work best on Google Chrome, otherwise will not be
+ * working as expected.
  */
 angular.module('ng-kabam-notification', [])
   .factory('WebNotification', function() {
