004028fd24dfc4c0354a197a939549e71226b280
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-12-12 23:53:11 +0700

    feat(Web Notification): Add click listener on Call type notification
    Notes:
    When the Call notification is clicked by callee, it will open up the room view. The example URI, /home#/call/room/34e971f0-7b67-4506-8f96-9855163477fc
    
    JIRA Issue: KAB-193

--- src/services/notification-factory.js
+++ src/services/notification-factory.js
@@ -8,47 +8,56 @@
  * Notification, the notification dialog will show up.
  */
 angular.module('ng-kabam-notification')
-  .factory('NotificationFactory', function() {
-    return {
-      createNotification: function(options) {
-        if (options.notificationType === 'broadcast') {
-          new Notification('Broadcast', {
-            body: options.data.message
-          });
-        } else if (options.notificationType === 'notify' &&
-          options.data.type && options.data.type === 'default') {
+  .factory('NotificationFactory', ['$window',
+    function() {
+      return {
+        createNotification: function(options) {
+          if (options.notificationType === 'broadcast') {
+            new Notification('Broadcast', {
+              body: options.data.message
+            });
+          } else if (options.notificationType === 'notify' &&
+            options.data.type && options.data.type === 'default') {
 
-          // required data payload structures
-          // {
-          //   user: {
-          //     username: 'user2'
-          //   },
-          //   message: {
-          //     from: 'user1',
-          //     text: 'Hi'
-          //   },
-          //   type: 'default'
-          // }
-          new Notification('Notification from ' + options.data.message.from, {
-            body: options.data.message.text
-          });
-        } else if (options.notificationType === 'notify' &&
-          options.data.type && options.data.type === 'call') {
-          // required data payload structures
-          // user: {
-          //   username: 'callee'
-          // },
-          // message: {
-          //   callee: 'callee',
-          //   caller: 'caller',
-          //   roomId: '34e971f0-7b67-4506-8f96-9855163477fc'
-          // },
-          // type: 'call'
-          new Notification('You got call!', {
-            body: options.data.message.caller + ' is calling. Click this notification to accept'
-          });
+            // required data payload structures
+            // {
+            //   user: {
+            //     username: 'user2'
+            //   },
+            //   message: {
+            //     from: 'user1',
+            //     text: 'Hi'
+            //   },
+            //   type: 'default'
+            // }
+            new Notification('Notification from ' + options.data.message.from, {
+              body: options.data.message.text
+            });
+          } else if (options.notificationType === 'notify' &&
+            options.data.type && options.data.type === 'call') {
+            // required data payload structures
+            // user: {
+            //   username: 'callee'
+            // },
+            // message: {
+            //   callee: 'callee',
+            //   caller: 'caller',
+            //   roomId: '34e971f0-7b67-4506-8f96-9855163477fc'
+            // },
+            // type: 'call'
+            var callNotification = new Notification('You got call!', {
+              body: options.data.message.caller + ' is calling. Click this notification to accept'
+            });
 
+            // when the notification clicked then redirect to room
+            callNotification.addEventListener('click', function() {
+              $window.open('/home#/call/room/' + data.message.roomId,
+                '_self');
+              notification.close();
+            });
+
+          }
         }
-      }
-    };
-  });
+      };
+    }
+  ]);
